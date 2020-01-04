4aca3390afcbd236f7846a5f01ccd1d7e0ce4d8e
Ayo Makanjuola <makville@gmail.com>
2020-01-04 23:39:34 +0100

    Made loaded Acl easier by checking if there's a global component before loading plugin component

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,12 +5,15 @@ namespace MakvilleMailer\Controller;
 use App\Controller\AppController as BaseController;
 
 class AppController extends BaseController {
-    
+
     public function initialize() {
         parent::initialize();
-        $this->loadComponent('MakvilleMailer.Mailer');
-        if(\Cake\Core\Plugin::isLoaded('MakvilleAcl')) {
-            $this->loadComponent('MakvilleAcl.Acl');
+        if (!in_array('Acl', $this->components()->loaded())) {
+            $this->loadComponent('MakvilleMailer.Mailer');
+            if (\Cake\Core\Plugin::isLoaded('MakvilleAcl')) {
+                $this->loadComponent('MakvilleAcl.Acl');
+            }
         }
     }
+
 }
