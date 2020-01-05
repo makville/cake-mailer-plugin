57a16374978064f691aa89c8eac595cbf38337f4
Ayo Makanjuola <makville@gmail.com>
2020-01-05 08:30:52 +0100

    Layout defaults set

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,12 +3,13 @@
 namespace MakvilleMailer\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Core\Configure;
 
 class AppController extends BaseController {
 
     public function initialize() {
         parent::initialize();
-        $this->viewBuilder()->setLayout('admin');
+        $this->viewBuilder()->setLayout(Configure::read('makville-mailer-layout', 'admin'));
         if (!in_array('Acl', $this->components()->loaded())) {
             $this->loadComponent('MakvilleMailer.Mailer');
             if (\Cake\Core\Plugin::isLoaded('MakvilleAcl')) {
